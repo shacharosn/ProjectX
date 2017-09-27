67a3ac3cd101fa65ffc26d62e670a702b02b56e1
shachar rosenman <shacharosn@gmail.com>
2017-09-27 21:28:35 +0300

    creating repository

---
+++ src/main/java/Study/ddddd.java
@@ -0,0 +1,7 @@
+package Study;
+
+public class ddddd {
+    public static void main(String[] args) {
+        System.out.println("sssss");
+    }
+}
---
+++ src/test/java/dvdvdvd.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class dvdvdvd {
+
+    @Test
+    public void myFirstTest()
+    {
+        Assert.assertTrue(1==2);
+    }
+
+}
